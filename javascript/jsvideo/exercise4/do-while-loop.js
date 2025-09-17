59b791b2201634839e479288d235af779d1ff6ba
JakeBissen <Joy303jake303@gmail.com>
2025-09-17 17:27:20 -0500

    working onpractice for section 2

---
+++ javascript/jsvideo/exercise4/do-while-loop.js
@@ -0,0 +1,20 @@
+
+
+let userInput = null;
+do{
+    userInput = promt('enter a value' );
+} while (userInput === null || userInput.length === 0); 
+    console.log(userInput + 'is valid');
+
+
+    /* De Morgan's Laws:
+//   !(A && B) <-->!A ||!B
+A B                           
+F F  T             T
+F T  T             T
+T F  T             T
+T T  F             F
+
+// !(A || B) --> !A && !B
+// Check this table for truth equality.
+*/
--- javascript/jsvideo/exercise4/iteration.js
+++ javascript/jsvideo/exercise4/iteration.js
@@ -6,4 +6,38 @@ let list = ['Sunday',
     'Friday', 
     'Saturday'
 ];
+// USE PYTHON.TUTOR for HELP!
+
+console.log('while loop: ');
+
+let i = 0
+while (i < list.length){
+    console.log(list[i]);
+    i = i +1
+
+}
+
+console.log('for loop: ');
+/// FOR LOOPS
+// for (initialization; condition; increment/decrement){ body_statements; }
+for(let i =0; i <list.length; i += 1){
+    console.log(list[i]);
+}
+
+console.log('for..in loop: ');
+for(let i in list){
+        console.log(list[i]);
+
+}
+
+
+
+
+console.log('for..of loop: ');
+for(let day of list){
+        console.log(day);
+
+}
+
+
 
