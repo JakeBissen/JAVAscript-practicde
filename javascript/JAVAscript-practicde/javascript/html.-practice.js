25f101894e62c4352cb69240427270ba8a34c58a
JakeBissen <Joy303jake303@gmail.com>
2025-09-22 12:30:44 -0500

    adding

---
+++ javascript/JAVAscript-practicde/javascript/html.-practice.js
@@ -0,0 +1,37 @@
+// let count = document.getElementById;
+// let plus = document.getElementById('btn1');
+// function add(){
+//     plus = 0;
+//     console.log([plus] + 1)
+
+// }
+
+//  let mins = document.getElementById('btn2');
+// function sub(){
+//     mins = 0;
+//     console.log([mins] - 1)
+// }
+
+// Wrong /\
+
+
+
+
+
+
+let count = document.getElementById('count');
+let counter = 0;
+
+let plus = document.getElementById('btn1');
+
+
+function add(){
+    count.innerText = ++counter;
+
+}
+
+ let mins = document.getElementById('btn2');
+function sub(){
+        count.innerText = --counter;
+
+}
--- javascript/JAVAscript-practicde/javascript/js-dom.js
+++ javascript/JAVAscript-practicde/javascript/js-dom.js
@@ -11,6 +11,8 @@
 // console.log(document.body);
 
 
+/// ------- SELECTING ELEMENTS -----////
+
 // getElementByID = 1
 let mainTitle = document.getElementById('main-title') 
 console.log(document.getElementById('main-title').innerText);
@@ -29,13 +31,59 @@ mainParagraph.innerHTML = '<i> Hellow using DOM with style! </i>';
 
 
 let cards = document.getElementsByClassName('card')
-//                    this becomes a list
+//                     this becomes a list
 console.log(cards[0])
 console.log(cards[1])
 
 for(let i = 0; i < cards.length; i++)
 {
-    cards[i].innerText = 'Card' + (i+1);
+    cards[i].innerText = 'Card ' + (i+1);
 }
+
+
+let listItems = document.getElementsByTagName('li');
+
+for(let i = 0; i < listItems.length; i++)
+{
+    listItems[i].innerText = 'Item ' + (i+1);
+    listItems[i].style.color = 'red';
+    listItems[i].style.backgroundColor = 'black';
+    listItems[i].style.margin = '10px';
+}
+
+let title= document.getElementById('title');
+let p = document.getElementById('desc');
+let input = document.getElementById('name');
+let image = document.getElementById('image');
+let buttom = document.getElementById('btn');
+
+console.log(input.value); //use value to get input value
+input.value = 'type in me';
+
+title.style.color = 'blue';
+title.style.fontFamily = 'Arial';
+
+console.log(image.src);
+
+image.src = 'https://www.boredpanda.com/blog/wp-content/uploads/2023/10/weird-hard-images-cover_800.jpg';
+image.style.width = '300px';
+image.style.height = '200px';
+
+image.setAttribute('alt', 'random image from internet')
+
+
+///  ----- HANDLING THE CLICK ---- ////
+
+function click(){
+    alert('button clicked');
+}
+
+
+
+
+//// ----- APPLYING(CREATING/REMOVING) ELEMENTS ---- /////
+
+
+
 
 
