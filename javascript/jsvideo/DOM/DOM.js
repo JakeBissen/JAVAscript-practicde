4542f50f54c548a54f40ff3681bea51ef496f1e0
JakeBissen <Joy303jake303@gmail.com>
2025-09-23 10:03:13 -0500

    commit

--- javascript/jsvideo/DOM/DOM.js
+++ javascript/jsvideo/DOM/DOM.js
@@ -1,7 +1,41 @@
 
-
 // if you use document 'document.querrySelectorAll' you have to have iteration on the result. Also, it is not an arry. BUT, you can easily turn it into one! 
 
+//                  let text = ' frist, second, third'
+// text.split(' '); -- gives you the words /\
+
+
+debugger;
+
+///1A. console log the content of the list items in seperate lines.
+
+// let list = document.getElementById('list');
+// console.log (list); -- bad
+
+let listItems = document.querySelectorAll('li'); 
+for(let item of listItems){
+    console.log(item.innerText);
+}
+
+///1B. console.log the first word of the second paragraph.
+
+// let para = document.querrySelectorAll('ipsum');
+// console.log(para); -- bad
+console.log('---------------------');
+
+let parapgraphs = document.querySelectorAll('p');
+let secondParagraph = parapgraphs[1]; 
+let secondParagraphText = secondParagraph.innerText;
+let firstWordOfSecondParagraphText = secondParagraphText.split(' ')[0];
+console.log(firstWordOfSecondParagraphText);
 
 
 
+console.log('---------------------');
+
+///1C. console log the number of parapgraphs.
+
+// let pars= document.querrySelectorTag(p);
+// console.log(pars); -- bad
+
+console.log(parapgraphs.length);
