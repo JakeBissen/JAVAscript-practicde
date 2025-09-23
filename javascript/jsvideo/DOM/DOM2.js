e98bee938bac2a404b8cd22ed8a0c4e1b7b14227
JakeBissen <Joy303jake303@gmail.com>
2025-09-23 17:26:10 -0500

    module 1 done

--- javascript/jsvideo/DOM/DOM2.js
+++ javascript/jsvideo/DOM/DOM2.js
@@ -48,8 +48,33 @@ function getMove() {
     );
 }
 
-const board = newArray(20).fill(null).map(
+const board = new Array(20).fill(null).map(
     () => new Array(20).fill(null).map(getMove)
 )
 
 
+const container = document.querySelector('.js-container');
+
+function renderRow(rowData){
+    let rowHtml = '<tr class="tic-tac-toe-row">';
+
+    for (let cell of rowData){
+        rowHtml += `<td class="tic-tac-toe-cell">${cell === null ? ' ' : cell}</td>`
+    }
+    rowHtml += '</tr>';
+    return rowHtml;
+}
+function renderBoard(board){
+    let html = '<table class="tic-tac-toe-board"> <tbody>';
+
+    for (let row of board){
+        html += renderRow(row);
+    }
+    html += '</tbody.</table>';
+    return html;
+}
+
+let html = renderBoard(board);
+
+
+container.innerHTML = html;
